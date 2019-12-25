b5e7e27bda54074f7a120b15dc68dc0cd129ee42
amk0928 <wlalsrb0928@gmail.com>
2019-12-26 02:41:35 +0900

    travis ci database

---
+++ config/travis.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS 'myapp_test'  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `board` (`id` int NOT NULL PRIMARY KEY auto_increment, `email` varchar(50) NOT NULL, `password` varchar(20) NOT NULL, `content` text, `createdAt` datetime, `updatedAt` datetime, `deleted` int unsigned);
+
