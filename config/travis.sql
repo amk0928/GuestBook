bc2f86b209fe82992b97f5e3fd9a546bd059dca8
amk0928 <wlalsrb0928@gmail.com>
2019-12-26 02:53:03 +0900

    change travis.sql'

--- config/travis.sql
+++ config/travis.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `guestbook`  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-USE `board`
+USE `guestbook`
 
 CREATE TABLE IF NOT EXISTS `board` (`id` int NOT NULL PRIMARY KEY auto_increment, `email` varchar(50) NOT NULL, `password` varchar(20) NOT NULL, `content` text, `createdAt` datetime, `updatedAt` datetime, `deleted` int unsigned);
 
