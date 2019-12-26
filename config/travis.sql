81e1bd37e80068c67b5ac2dad9a211ee0bb8edef
amk0928 <wlalsrb0928@gmail.com>
2019-12-26 12:56:25 +0900

    remove database password

--- config/travis.sql
+++ config/travis.sql
@@ -5,3 +5,4 @@ USE `guestbook`
 CREATE TABLE IF NOT EXISTS `board` (`id` int NOT NULL PRIMARY KEY auto_increment, `email` varchar(50) NOT NULL, `password` varchar(20) NOT NULL, `content` text, `createdAt` datetime, `updatedAt` datetime, `deleted` int unsigned);
 
 INSERT INTO `board`(`email`, `password`, `content`, `createdAt`, `updatedAt`, `deleted`) VALUES("test@test.com", "1234", "test", NOW(), NOW(), 1);
+INSERT INTO `BOARD`(`email`, `password`, `content`, `createdAt`, `updatedAt`, `deleted`) VALUES("test@test.com", "1234", "test", NOW(), NOW(), 1);
